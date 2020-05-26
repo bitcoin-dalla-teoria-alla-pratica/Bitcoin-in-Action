afdd94677c87c60df8881498ae5cd3311bad9d7d
Barno <barno77@gmail.com>
2020-05-26 15:20:52 +0200

    fix video 3

--- Video 3 – The Times e Halving/blocco_genesi.sh
+++ Video 3 – The Times e Halving/blocco_genesi.sh
@@ -1,11 +1,11 @@
-bitcoin-cli getblockhash 0
+BLOCK_HASH=$(bitcoin-cli getblockhash 0)
 
-bitcoin-cli getblock 000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f
+bitcoin-cli getblock $BLOCK_HASH
 
 #error
 bitcoin-cli getrawtransaction 4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b 2
 
-bitcoin-cli getblock 000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f 2
+bitcoin-cli getblock $BLOCK_HASH 2
 
 #Hex to ASCII
 echo 04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73 | xxd -r -p
